8aa340ae0c6679e8a202811eb0b5950711b0832e
Trevor Foster <trevafoser@gmail.com>
2023-12-12 22:09:02 -0600

    Add Schemas

---
+++ models/clothingItems.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const clothingItem = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  weather: {
+    type: String,
+    enum: ["hot", "warm", "cold"],
+  },
+  imageURL: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: "You must enter a valid URL",
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "users",
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("clothingItems", clothingItem);
