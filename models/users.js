55abef46382fdcdb109ec7292cfc8236e6690cba
Trevor Foster <trevafoser@gmail.com>
2024-01-14 14:45:46 -0600

    Complete Step 1:
    Add email and password schema

--- models/users.js
+++ models/users.js
@@ -16,6 +16,21 @@ const user = new mongoose.Schema({
       message: "You must enter a valid URL",
     },
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: "you must enter a valid Email",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
 });
 
 module.exports = mongoose.model("users", user);
