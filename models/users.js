5aaf974c1feb313fd26e50b2e48aa2f1fae8ffdb
Trevor Foster <trevafoser@gmail.com>
2024-01-14 22:25:12 -0600

    Complete Step 9

--- models/users.js
+++ models/users.js
@@ -28,8 +28,8 @@ const user = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    minlength: 2,
-    maxlength: 30,
+    minlength: 8,
+    select: false,
   },
 });
 
@@ -37,18 +37,20 @@ userSchema.statics.findUserByCredentials = function findUserByCredentials(
   email,
   password,
 ) {
-  return this.findOne({ email }).then((user) => {
-    if (!user) {
-      return Promise.reject(new Error("Incorrect email or password"));
-    }
-
-    return bcrypt.compare(password, user.password).then((matched) => {
-      if (!matched) {
+  return this.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
         return Promise.reject(new Error("Incorrect email or password"));
       }
-      return user;
+
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error("Incorrect email or password"));
+        }
+        return user;
+      });
     });
-  });
 };
 
 module.exports = mongoose.model("users", user);
