c0104e681a1990139035f76f377ce5aa14b52807
Trevor Foster <trevafoser@gmail.com>
2024-01-15 20:50:54 -0600

    Fix: auth.js revisions

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,8 +1,9 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
+const { INVALID_CREDENTIALS } = require("../utils/errors");
 
 const handleAuthError = (res) => {
-  res.status(401).send({ message: "Authorization Error" });
+  res.status(INVALID_CREDENTIALS).send({ message: "Authorization Error" });
 };
 
 const extractBearerToken = (header) => header.replace("Bearer ", "");
